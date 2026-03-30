faabb8025e5ac73363a2cc22fa03a4909a33d07c
BlesUstetu <lionustetu@gmail.com>
2026-03-30 21:19:16 +0900

    Add files via upload

---
+++ src/ChatAI.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { parseSignal } from '../lib/parser'
+import { useTradingStore } from '../store/tradingStore'
+
+export default function ChatAI() {
+  const [message, setMessage] = useState('')
+  const [chat, setChat] = useState([])
+
+  const addOrder = useTradingStore((s) => s.addOrder)
+
+  const sendMessage = () => {
+    if (!message) return
+
+    const parsed = parseSignal(message)
+
+    if (parsed) {
+      addOrder(parsed)
+    }
+
+    setChat([...chat, { text: message }])
+    setMessage('')
+  }
+
+  return (
+    <div>
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Enter signal..."
+      />
+      <button onClick={sendMessage}>Send</button>
+
+      <div>
+        {chat.map((c, i) => (
+          <div key={i}>{c.text}</div>
+        ))}
+      </div>
+    </div>
+  )
+}
