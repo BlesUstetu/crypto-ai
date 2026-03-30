70c94d6ba549de97ddeb1c924e4660ebc3c6b627
BlesUstetu <lionustetu@gmail.com>
2026-03-30 16:39:47 +0700

    Update market.js

--- api/market.js
+++ api/market.js
@@ -1,11 +1,28 @@
-
-import { getMarketData } from "../lib/market.js";
-
 export default async function handler(req, res) {
   try {
-    const data = await getMarketData();
-    res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
+    const response = await fetch(
+      "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+    )
+
+    if (!response.ok) {
+      return res.status(500).json({
+        error: "Failed to fetch Binance API",
+      })
+    }
+
+    const data = await response.json()
+
+    return res.status(200).json({
+      price: data.price,
+      symbol: data.symbol,
+    })
+
+  } catch (error) {
+    console.error("API ERROR:", error)
+
+    return res.status(500).json({
+      error: "Internal Server Error",
+      detail: error.message,
+    })
   }
 }
