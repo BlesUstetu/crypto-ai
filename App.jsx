d79f3d5c386f31bf391846377bf430aba64229f9
BlesUstetu <lionustetu@gmail.com>
2026-03-30 21:15:15 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import ChatAI from './components/ChatAI'
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Crypto AI</h1>
+      <ChatAI />
+    </div>
+  )
+}
