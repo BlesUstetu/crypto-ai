a1a1ad2584b0c0bcf264fd95a182df450d5e0eb4
BlesUstetu <lionustetu@gmail.com>
2026-04-04 15:40:59 +0700

    Update orderbookWS.js

--- src/utils/orderbookWS.js
+++ src/utils/orderbookWS.js
@@ -1,11 +1,25 @@
 let ws;
+
 export function startOrderBook(symbol, onUpdate) {
   if (ws) ws.close();
-  ws = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@depth20@100ms`);
+
+  ws = new WebSocket(
+    `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@depth20@100ms`
+  );
+
   ws.onmessage = (e) => {
     const d = JSON.parse(e.data);
-    const bids = d.bids.map(b => ({ price: +b[0], size: +b[1] }));
-    const asks = d.asks.map(a => ({ price: +a[0], size: +a[1] }));
+
+    const bids = d.bids.map(b => ({
+      price: +b[0],
+      size: +b[1]
+    }));
+
+    const asks = d.asks.map(a => ({
+      price: +a[0],
+      size: +a[1]
+    }));
+
     onUpdate({ bids, asks });
   };
 }
