e73ed8a16e6b42343c0a852c67b76acb613f672f
BlesUstetu <lionustetu@gmail.com>
2026-04-04 15:35:56 +0700

    Update aggregate.js

--- api/aggregate.js
+++ api/aggregate.js
@@ -1,3 +1,14 @@
-export default async function handler(req,res){
-  res.json({ price:65000, delta:100 });
+export default async function handler(req, res) {
+  const data = {
+    price: 65000,
+    buy: 1200,
+    sell: 800,
+    bid: 5000,
+    ask: 3000
+  };
+
+  const delta = data.buy - data.sell;
+  const imbalance = data.bid / data.ask;
+
+  res.json({ ...data, delta, imbalance });
 }
