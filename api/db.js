aedd922cf41f510b5e9836f3047996924b85f372
BlesUstetu <lionustetu@gmail.com>
2026-04-04 15:52:15 +0700

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,10 +1,9 @@
-import fs from "fs";
-const FILE="./data/trades.json";
-export function saveTrade(t){
-  let d=[]; if(fs.existsSync(FILE)) d=JSON.parse(fs.readFileSync(FILE));
-  d.push(t); fs.writeFileSync(FILE,JSON.stringify(d,null,2));
+let trades = [];
+
+export function saveTrade(t) {
+  trades.push(t);
 }
-export function getTrades(){
-  if(!fs.existsSync(FILE)) return [];
-  return JSON.parse(fs.readFileSync(FILE));
+
+export function getTrades() {
+  return trades;
 }
